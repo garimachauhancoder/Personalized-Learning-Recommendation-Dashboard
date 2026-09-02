1897269f3dd162e2a11921b347f0044f6acb684e
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-09-02 19:28:16 +0530

    Database file updated

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-    auocommit=False,
+    autocommit=False,
     autoflush=False,
     bind=engine
 )
