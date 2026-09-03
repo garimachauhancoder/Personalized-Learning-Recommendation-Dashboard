92c7c8cc032d4767cdc66117c44e554cce6d9e86
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-09-03 15:20:56 +0530

    main file updated

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+from sqlalchemy import text
+from backend.database import engine
 
 app = FastAPI(title="Personalised Learning Recommendation System")
 
@@ -7,3 +9,11 @@ def home():
     return {
         "message": "Personalised Learning API is running"
     }
+
+@app.get("/test-db")
+def test_database():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {
+            "database_connected": result.scalar() == 1
+        }
