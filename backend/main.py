47c1e6f9e8826e35b7b09fa670f08dbe12bba6b1
Garima Chauhan <garimachauhan2301@gmail.com>
2026-09-19 21:39:30 +0530

    Main file updated

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from backend.database import engine
 from backend.routes.users import router as users_router
 from backend.routes.topics import router as topics_router
 from backend.routes.learning_sessions import router as learning_sessions_router
-
+from backend.routes.assessments import router as assessments_router
 
 app = FastAPI(title="Personalised Learning Recommendation System")
 
@@ -25,3 +25,4 @@ def test_database():
 app.include_router(users_router)
 app.include_router(topics_router)
 app.include_router(learning_sessions_router)
+app.include_router(assessments_router)
